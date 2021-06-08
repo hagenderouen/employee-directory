92c0dc03db83cbc099f87a0d7fddaed03787753f
Hagen DeRouen <hagend87@gmail.com>
2021-06-08 17:06:26 -0500

    Add random user API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import '@fontsource/roboto';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from './components/Grid';
-import userData from './userDataRes.json';
 
 function App() {
   return (
@@ -12,10 +10,10 @@ function App() {
       <CssBaseline />
       <AppBar position="static">
         <Toolbar>
-          <Typography><h2>Employee Directory</h2></Typography>
+          <Typography>Employee Directory</Typography>
         </Toolbar>
       </AppBar>
-      <Grid userData={userData} />
+      <Grid/>
     </React.Fragment>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataGrid, GridToolbar } from '@material-ui/data-grid';
+import userAPI from '../utils/userAPI';
+import userDemoData from '../userDataRes.json';
 
 const columns = [
   { field: "id", headerName: "Employee ID", flex: 1},
@@ -8,23 +10,34 @@ const columns = [
   { field: "email", headerName: "Email", flex: 1}
 ];
 
-export default function Grid(props) {
-  return (
-    <div style={{ height: 300, width: "100%" }}>
-      <DataGrid
-      components={{
-          Toolbar: GridToolbar
-      }}
-      rows={props.userData.results.map(user => (
-        {
-            id: user.id.value,
-            firstName: user.name.first,
-            lastName: user.name.last,
-            email: user.email
-        }
-      ))} 
-      columns={columns} 
-      />
-    </div>
-  );
+function Grid() {
+
+    const [users, setUsers] = useState(userDemoData);
+
+    useEffect(() => {
+        userAPI.get()
+        .then(res => setUsers({ results: res.data.results }))
+        .catch(err => console.log(err));
+    }, []);
+
+    return (
+        <div style={{ height: 500, width: "100%" }}>
+        <DataGrid
+        components={{
+            Toolbar: GridToolbar
+        }}
+        rows={users.results.map((user, idx) => (
+            {
+                id: idx,
+                firstName: user.name.first,
+                lastName: user.name.last,
+                email: user.email
+            }
+        ))} 
+        columns={columns} 
+        />
+        </div>
+    );
 }
+
+export default Grid;
